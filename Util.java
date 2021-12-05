d25379f8d488f9e1622651650bf631833c5aba16
milind-u <milind.upadhyay@gmail.com>
2021-12-05 11:23:23 -0800

    Fix check order in wrap

--- Util.java
+++ Util.java
@@ -5,7 +5,7 @@ public class Util {
   private Util() {} // Don't let anyone instantiate
 
   public static int wrap(int i, int max) {
-    Log.checkGt(0, max, "Max must be positive");
+    Log.checkGt(max, 0, "Max must be positive");
     if (i < 0) {
       i = max - i;
     }
