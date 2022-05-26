42dfcb763f32891d3aca51b04a5b8d613f05bd38
Milind Upadhyay <milind.upadhyay@gmail.com>
2022-05-26 09:40:47 -0700

    Add playing checker to audio

--- Audio.java
+++ Audio.java
@@ -2,7 +2,6 @@ package swinglib;
 
 import java.io.File;
 import java.io.IOException;
-
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
@@ -11,6 +10,7 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 
 /**
  * A class to play audio clips.
+ * 
  * @author milind
  */
 public class Audio {
@@ -19,6 +19,7 @@ public class Audio {
 
   /**
    * Creates an audio object with the given file.
+   * 
    * @param path Path to audio file
    */
   public Audio(String path) {
@@ -31,19 +32,25 @@ public class Audio {
       c = AudioSystem.getClip();
       AudioInputStream audio = AudioSystem.getAudioInputStream(new File(path));
       c.open(audio);
-    } catch (LineUnavailableException | IOException
-        | UnsupportedAudioFileException e) {
+    } catch (LineUnavailableException | IOException | UnsupportedAudioFileException e) {
       e.printStackTrace();
     }
     return c;
   }
 
   /**
-   * Plays the audio clip. 
+   * Plays the audio clip.
    */
   public void play() {
     clip.start();
     clip.setFramePosition(0);
+  }
+
+  /*
+   * Returns whether the clip is playing
+   */
+  public boolean playing() {
+    return clip.isActive();
   }
 
 }
